3429bb854ff1abc3ebac37998e8c3c559e64a188
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 16:47:40 +0300

    Created TestGenerator class. ITestGenerator inherited, added empty implementation.

---
+++ TestGenerator/TestGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    public class TestGenerator : ITestGenerator
+    {
+        public TestGenerator()
+        {
+        }
+
+        public Task Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
