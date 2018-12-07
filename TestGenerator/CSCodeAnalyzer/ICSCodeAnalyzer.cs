82efcfc1075719c311dcfbcb58063d49d429a402
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 05:13:37 +0300

    Interface implemented.

--- TestGenerator/CSCodeAnalyzer/ICSCodeAnalyzer.cs
+++ TestGenerator/CSCodeAnalyzer/ICSCodeAnalyzer.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestGenerator.CSCodeDataStructures;
 
 namespace TestGenerator.CSCodeAnalyzer
 {
     public interface ICSCodeAnalyzer
     {
+        FileInformation AnalyzeCode(string code);
     }
 }
