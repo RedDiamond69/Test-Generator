898d2bd843548a5039bb5bc86ab2b35dc7375010
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 04:48:27 +0300

    Type and PathInformation class implemented.

--- TestGenerator/CSCodeDataStructures/PathInformation.cs
+++ TestGenerator/CSCodeDataStructures/PathInformation.cs
@@ -8,5 +8,15 @@ namespace TestGenerator.CSCodeDataStructures
 {
     public class PathInformation
     {
+        public string Path { get; protected set; }
+        public string Data { get; protected set; }
+
+        public PathInformation(string path, string data)
+        {
+            if ((path == null) || (data == null))
+                throw new ArgumentException("Arguments can't be null!");
+            Path = path;
+            Data = data;
+        }
     }
 }
--- TestGenerator/CSCodeDataStructures/TypeInformation.cs
+++ TestGenerator/CSCodeDataStructures/TypeInformation.cs
@@ -8,5 +8,13 @@ namespace TestGenerator.CSCodeDataStructures
 {
     public class TypeInformation
     {
+        public string Typename { get; protected set; }
+        public bool IsInterface { get; protected set; }
+
+        public TypeInformation(string typename)
+        {
+            Typename = typename ?? throw new ArgumentException("Typename can't be null!");
+            IsInterface = typename.StartsWith("I");
+        }
     }
 }
