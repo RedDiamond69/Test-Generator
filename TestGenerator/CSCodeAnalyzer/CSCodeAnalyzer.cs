bf0afa5475db6b45c624007136042f5808cf2c57
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 05:22:01 +0300

    ICSCodeAnalyzer interface inherited, added empty implementation.

---
+++ TestGenerator/CSCodeAnalyzer/CSCodeAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGenerator.CSCodeDataStructures;
+
+namespace TestGenerator.CSCodeAnalyzer
+{
+    public class CSCodeAnalyzer : ICSCodeAnalyzer
+    {
+        public CSCodeAnalyzer()
+        {
+        }
+
+        public FileInformation AnalyzeCode(string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
