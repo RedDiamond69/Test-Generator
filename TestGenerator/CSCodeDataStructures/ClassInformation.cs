d68d58f934695873c34097c67d739bf6fa5775d5
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 04:23:49 +0300

    ClassInformation class implemented.

--- TestGenerator/CSCodeDataStructures/ClassInformation.cs
+++ TestGenerator/CSCodeDataStructures/ClassInformation.cs
@@ -8,5 +8,19 @@ namespace TestGenerator.CSCodeDataStructures
 {
     public class ClassInformation
     {
+        public List<MethodInformation> MethodsDeclaration { get; protected set; }
+        public string NamespacesDeclaration { get; protected set; }
+        public string NamesDeclaration { get; protected set; }
+        public ConstructorInformation ConstructorsDeclaration { get; protected set; }
+
+        public ClassInformation(string name, string namespaceDeclaration, ConstructorInformation constructorInformation)
+        {
+            if ((name == null) || (namespaceDeclaration == null) || (constructorInformation == null))
+                throw new ArgumentException("Arguments can't be null!");
+            NamesDeclaration = name;
+            NamespacesDeclaration = namespaceDeclaration;
+            MethodsDeclaration = new List<MethodInformation>();
+            ConstructorsDeclaration = constructorInformation;
+        }
     }
 }
