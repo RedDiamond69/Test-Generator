0a0a49168fb558ad333f5a9486567437f186fb36
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 04:37:43 +0300

    FileInformation class implemented.

--- TestGenerator/CSCodeDataStructures/FileInformation.cs
+++ TestGenerator/CSCodeDataStructures/FileInformation.cs
@@ -8,5 +8,13 @@ namespace TestGenerator.CSCodeDataStructures
 {
     public class FileInformation
     {
+        public List<string> UsingsDeclaration { get; protected set; }
+        public List<ClassInformation> ClassesDeclaration { get; protected set; }
+
+        public FileInformation()
+        {
+            UsingsDeclaration = new List<string>();
+            ClassesDeclaration = new List<ClassInformation>();
+        }
     }
 }
