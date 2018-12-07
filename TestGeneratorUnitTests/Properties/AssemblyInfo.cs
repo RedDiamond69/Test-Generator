7a9acd20850a4198e4ecb6f8f1410a85f4e1f87d
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-07 03:44:46 +0300

    Created project structure.

---
+++ TestGeneratorUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestGeneratorUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestGeneratorUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e31fb0c8-1a1f-403d-9226-42f9b1a039bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestGeneratorUnitTests/TestGeneratorUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestGeneratorUnitTests
+{
+    [TestClass]
+    public class TestGeneratorUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
